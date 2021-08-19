7c9622de7b2a5e489ba8d5a6b91689deeabe0635
KojiKobayashi <k.kobayashi@33i.co.jp>
2021-08-19 11:15:33 +0900

    ~20th nock

--- 本章/2章/2.py
+++ 本章/2章/2.py
@@ -70,4 +70,117 @@ month_data.describe()
 
 # %%
 month_data.sum()
+
+# %%
+'''
+14th nock
+月別推移の可視化
+'''
+
+# %%
+import matplotlib.pyplot as pyplot
+%matplotlib inline
+month_data.sum().plot()
+
+# %%
+
+month_data.mean().plot()
+# %%
+'''
+15th nock
+'''
+
+# %%
+# ヒストグラム作成
+pyplot.hist(analyze_data['total_amount'])
+
+# %%
+pyplot.hist(analyze_data['total_amount'], bins=21)
+
+# %%
+'''
+16th nock
+'''
+
+# %%
+# 都道府県と月別売り上げでクロス集計作成
+pre_data = pd.pivot_table(analyze_data, index='order_accept_month', columns='narrow_area', values='total_amount', aggfunc='mean')
+pre_data
+
+# %%
+import japanize_matplotlib
+
+# %%
+print(pre_data.columns)
+for area in pre_data.columns:
+    print(area)
+    pyplot.plot(list(pre_data.index), pre_data[area], label=area)
+pyplot.legend()
+
+# %%
+'''
+17th nock
+店舗別集計
+'''
+
+# %%
+store_clustering = analyze_data.groupby('store_id').agg(['size', 'mean', 'median', 'max', 'min'])['total_amount']
+store_clustering.reset_index(inplace=True, drop=True)
+print(len(store_clustering))
+store_clustering.head()
+
+# %%
+import seaborn as sns
+hexbin = sns.jointplot(x='mean', y='size', data=store_clustering, kind='hex')
+
+# %%
+analyze_data.dtypes
+# %%
+'''
+18th nock
+'''
+
+# %%
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+
+# %%
+# 標準化
+sc = StandardScaler()
+store_clustering_sc = sc.fit_transform(store_clustering)
+
+# クラスタリング
+kmeans = KMeans(n_clusters=4, random_state=0)
+clusters = kmeans.fit(store_clustering_sc)
+store_clustering['cluster'] = clusters.labels_
+print(store_clustering['cluster'].unique())
+store_clustering.head()
+
+# %%
+'''
+19th nock
+'''
+
+# %%
+store_clustering.columns = ['月内売り上げ', '月内平均値', '月内中央値', '月内最大値', '月内最小値', 'cluster']
+store_clustering.groupby('cluster').count()
+
+# %%
+store_clustering.groupby('cluster').mean()
+
+# %%
+'''
+20th nock
+'''
+# %%
+from sklearn.manifold import TSNE
+tsne = TSNE(n_components=2, random_state=0)
+x = tsne.fit_transform(store_clustering_sc)
+tsne_df = pd.DataFrame(x)
+tsne_df['cluster'] = store_clustering['cluster']
+tsne_df.columns = ['axis_0', 'axis_1', 'cluster']
+tsne_df.head()
+
+# %%
+tsne_graph = sns.scatterplot(x = 'axis_0', y='axis_1', hue='cluster', data=tsne_df)
 # %%
