e1fc253af70f4f11cc0b0c3ada984db2ad18f564
KojiKobayashi <k.kobayashi@33i.co.jp>
2021-12-13 20:39:23 +0900

    add 63th knock

--- 本章/7章/7.py
+++ 本章/7章/7.py
@@ -3,6 +3,8 @@
 
 # %%
 import os
+
+from scipy.sparse.construct import random
 data_dir = 'data'
 input_dir = os.path.join(data_dir, '0_input')
 output_dir = os.path.join(data_dir, '1_output')
@@ -30,4 +32,17 @@ del ml_data['year_month']
 del ml_data['store_name']
 ml_data = pd.concat([ml_data, category_data], axis=1)
 ml_data.columns
+
+# %%
+from sklearn.model_selection import train_test_split
+train_data, test_data = train_test_split(ml_data, test_size=0.3, random_state=0)
+print(f'Train count:{len(train_data)}/ Test count:{len(test_data)}')
+print(f'Weekday Train0: {len(train_data.loc[train_data["y_weekday"]==0])}')
+print(f'Weekday Train1: {len(train_data.loc[train_data["y_weekday"]==1])}')
+print(f'Weekday Test0: {len(test_data.loc[test_data["y_weekday"]==0])}')
+print(f'Weekday Test1: {len(test_data.loc[test_data["y_weekday"]==1])}')
+print(f'Weekend Train0: {len(train_data.loc[train_data["y_weekend"]==0])}')
+print(f'Weekend Train1: {len(train_data.loc[train_data["y_weekend"]==1])}')
+print(f'Weekend Test0: {len(test_data.loc[test_data["y_weekend"]==0])}')
+print(f'Weekend Test1: {len(test_data.loc[test_data["y_weekend"]==1])}')
 # %%
